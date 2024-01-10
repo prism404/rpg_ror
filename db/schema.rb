88d3731a5973e27fb2c019e64ae4fc6f977e4910
prism404 <veronique.vdang@gmail.com>
2024-01-10 13:21:06 +0100

    log schema modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_09_045042) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_115811) do
   create_table "emails", force: :cascade do |t|
     t.string "password_digest"
     t.datetime "created_at", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_09_045042) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "health_points"
+    t.string "avatar"
   end
 
   create_table "quests", force: :cascade do |t|
